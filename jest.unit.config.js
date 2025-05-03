4b44297acaf202c39fe9eeeb83e4824dfe309888
Ran Yitzhaki <ranyitz@gmail.com>
2025-05-03 14:41:53 +0200

    fix build

--- jest.unit.config.js
+++ jest.unit.config.js
@@ -1,5 +1,7 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
-export default {
+
+// eslint-disable-next-line no-undef
+module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   testMatch: ["**/tests/unit/**/*.test.ts"],
